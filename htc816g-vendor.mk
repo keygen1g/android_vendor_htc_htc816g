60b4bcc45e773504b99627c221142819b3fd8988
Ahmed Hady <ahmedhady6@gmail.com>
2016-06-04 13:03:48 +0200

    HTC 816G dual sim Vendor files
    These Vendor files based on @fire855 and @ferhung-mtk Vendor files (Thanks for them)

---
+++ htc816g-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc816g/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc816g/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc816g/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc816g/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc816g/proprietary/vendor,system/vendor)
